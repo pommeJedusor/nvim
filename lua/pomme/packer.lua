519cecdb27f690b9d0ff13c4d3012bee6fc03ad1
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-21 17:57:57 +0200

    nvim-tree

--- lua/pomme/packer.lua
+++ lua/pomme/packer.lua
@@ -1,52 +1,55 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim' 
+	use("wbthomason/packer.nvim")
 
 	-- telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
 	-- theme manager
-	use 'zaldih/themery.nvim'
+	use("zaldih/themery.nvim")
 
 	-- themes
-	use 'folke/tokyonight.nvim'
-	use 'Mofiqul/dracula.nvim'
-	use 'shaunsingh/nord.nvim'
-	use 'maxmx03/solarized.nvim'
-	use 'navarasu/onedark.nvim'
-	use 'tanvirtin/monokai.nvim'
-	use { "catppuccin/nvim", as = "catppuccin" }
-	use 'marko-cerovac/material.nvim'
-	use 'mhartington/oceanic-next'
+	use("folke/tokyonight.nvim")
+	use("Mofiqul/dracula.nvim")
+	use("shaunsingh/nord.nvim")
+	use("maxmx03/solarized.nvim")
+	use("navarasu/onedark.nvim")
+	use("tanvirtin/monokai.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
+	use("marko-cerovac/material.nvim")
+	use("mhartington/oceanic-next")
 
 	-- tree-sitter
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
 	-- lsp-zero
-	use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-	use({'neovim/nvim-lspconfig'})
-	use({'hrsh7th/nvim-cmp'})
-	use({'hrsh7th/cmp-nvim-lsp'})
-	use {
+	use({ "VonHeikemen/lsp-zero.nvim", branch = "v4.x" })
+	use({ "neovim/nvim-lspconfig" })
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use({
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-	}
+	})
 
-    -- formater
-    use({
-        "stevearc/conform.nvim",
-        config = function()
-            require("conform").setup()
-        end,
-    })
+	-- formater
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup()
+		end,
+	})
 
+	-- tree file
+	use("nvim-tree/nvim-tree.lua")
 end)
