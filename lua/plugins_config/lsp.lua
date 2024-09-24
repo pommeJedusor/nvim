d13330d8d259a912ad2120adfc29df93384c1bb6
pommejedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-09-24 19:59:17 +0200

    add py and ts lsp

--- lua/plugins_config/lsp.lua
+++ lua/plugins_config/lsp.lua
@@ -1,7 +1,7 @@
 -- lsp config
 require("mason").setup({})
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls" },
+	ensure_installed = { "lua_ls", "ts_ls", "pyright" },
 	handlers = {
 		function(server_name)
 			require("lspconfig")[server_name].setup({})
